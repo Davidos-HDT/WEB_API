511933ed74d0515f4abe6a7167bd4b370e8de99f
Davidos <david.solobub@gmail.com>
2021-12-27 16:05:40 +0200

    Web Api

---
+++ Models/Games.cs
@@ -0,0 +1,9 @@
+﻿namespace WEB_API.Models
+{
+    public class Games
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+    }
+}
---
+++ Models/GamesContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WEB_API.Models
+{
+    public class GamesContext : DbContext
+    {
+        public DbSet<Games> Games { get; set; }
+        public GamesContext(DbContextOptions<GamesContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
